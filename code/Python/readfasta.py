3d0f1d98d52571deb7de623dfb9ba222d3935758
SnorriHallgrims <snorri16@ru.is>
2019-02-20 17:22:12 +0000

    Dagslok

--- code/Python/readfasta.py
+++ code/Python/readfasta.py
@@ -1,7 +1,7 @@
 from Bio import SeqIO
 import os.path
 
-def readfasta():
+def fread():
 
     sp=['0.05','0.25','0.50','0.75','1.00','1.25','1.50','1.75','2.00']
     seqdict={}
@@ -19,6 +19,7 @@ def readfasta():
         fasta_seq = SeqIO.parse(filepath,'fasta')
 
         for r in fasta_seq:
-            seqdict[r.id][s]=r.seq
+            seqdict[r.id][s]=str(r.seq)
 
     return seqdict
+
--- code/R/mQtracking.R
+++ code/R/mQtracking.R
@@ -9,9 +9,23 @@ dropout_n_sub=dropout_n[401:500]
 dropout=subset(mdata,qName %in% dropout_n)
 dropout_sub=subset(mdata,qName %in% dropout_n_sub)
 
+dropout_p=list()
+counter=0
+for (i in dropout_n)
+{
+  counter=counter+1
+  subsp=subset(mdata,qName==i)
+  sum1=sum(subsp[['mapQual']])
+  if (sum1!=(length(subsp[['mapQual']])*(-1))){dropout_p=append(dropout_p,i)}
+}
+
+dropoutq=subset(dropout,mapQual>50)
+dropoutq=dropoutq[['qName']]
+dropoutq=unique(dropoutq)
+
 p1 <- ggplot(mdata,aes(x=sp,y=mapQual))+
   theme_light()+
   theme(text = element_text(family = "Cambria"))+
-  geom_point(data=dropout_sub)#,aes(colour=qName))
+  geom_point(data=subset(mdata,qName %in% dropoutq[1:4]),aes(colour=qName))
 
 plot(p1)
